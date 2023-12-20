7cb8fdd0ef837954b94183621d3827fed2ffe512
Lucas al Jalali <lucasaljalali@gmail.com>
2023-12-20 12:46:46 +0000

    WIP: design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AuthProvider } from "./contexts/AuthContext";
+import { StyledEngineProvider } from "@mui/material";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,10 +13,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
-      <html lang="en" suppressHydrationWarning={true}>
-        <body className={inter.className}>{children}</body>
-      </html>
-    </AuthProvider>
+    <StyledEngineProvider injectFirst>
+      <AuthProvider>
+        <html lang="en" suppressHydrationWarning={true}>
+          <body className={inter.className}>{children}</body>
+        </html>
+      </AuthProvider>
+    </StyledEngineProvider>
   );
 }
